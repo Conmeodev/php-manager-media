acdd60f2eee3d3ef0ac84ead1544e3912fdf7f8e
Conmeodev <seluonolai@gmail.com>
2025-04-03 12:27:51 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', '0');
-ini_set('display_startup_errors', '0');
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 $DOCUMENT_ROOT = isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : "C:/xampp/htdoc";
 $root = $DOCUMENT_ROOT.'/';
